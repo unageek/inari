6ecc53632bc2b7550b769658ae2e50c2259ce5bc
Masashi Mizuno <29035331+unageek@users.noreply.github.com>
2021-05-26 23:23:38 +0900

    Oops, docs.rs runs on x86-64

--- src/simd.rs
+++ src/simd.rs
@@ -1,13 +1,13 @@
 cfg_if::cfg_if! {
-    if #[cfg(target_arch = "aarch64")] {
+    if #[cfg(docsrs)] {
+        mod unimplemented;
+        pub(crate) use unimplemented::*;
+    } else if #[cfg(target_arch = "aarch64")] {
         mod aarch64;
         pub(crate) use aarch64::*;
     } else if #[cfg(target_arch = "x86_64")] {
         mod x86_64;
         pub(crate) use x86_64::*;
-    } else if #[cfg(docsrs)] {
-        mod unimplemented;
-        pub(crate) use unimplemented::*;
     } else {
         unreachable!();
     }
